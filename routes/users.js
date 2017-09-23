a842cde93e60d0334bcfa902c501e8296b4ca7ef
Leo <jamesleotucker@gmail.com>
2017-09-23 10:56:34 -0600

    Updated user route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Get all Users route');
+});
+router.post('/', function(req, res, next) {
+  res.send('Create User route');
+});
+router.get('/:id', function(req, res, next) {
+  res.send('Get one User route');
+});
+router.patch('/:id', function(req, res, next) {
+  res.send('Edit User route');
+});
+router.delete('/:id', function(req, res, next) {
+  res.send('Delete users route');
 });
 
 module.exports = router;
